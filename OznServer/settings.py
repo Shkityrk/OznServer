fadf2a34e84acb072b37a3dd941e1f4869f5f10e
Shkityr Konstantin <shkityr.k@yandex.ru>
2024-05-24 12:20:34 +0300

    upd settings.py

--- OznServer/settings.py
+++ OznServer/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TESSERACT_PATH = 'C:\\Program Files\\Tesseract-OCR'
+# TESSERACT_PATH = 'C:\\Program Files\\Tesseract-OCR'
+TESSERACT_PATH = '/usr/bin'
 
 
 # Quick-start development settings - unsuitable for production
